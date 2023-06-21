1ab78e90aa8ffcd48d391a3300ef94781ddfae72
gustavodscruz <gustavodiasdsc@gmail.com>
2023-06-21 12:01:54 -0300

    Initial commit

---
+++ src/main/java/org/example/Triangulo.java
@@ -0,0 +1,35 @@
+package org.example;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Scanner;
+
+public class Triângulo {
+    private double base = 10;
+    private double altura = 5;
+
+    public Triângulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    @Test
+    public double calcularArea(){
+        return(base * altura)/2;
+
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double base = 10.00;
+        double altura = 5;
+
+        Triângulo tri1 = new Triângulo(base, altura);
+
+
+
+        System.out.println(tri1.calcularArea());
+
+
+    }
+}
+
